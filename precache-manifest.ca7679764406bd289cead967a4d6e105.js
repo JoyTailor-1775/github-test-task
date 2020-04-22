452d34d35ffa96a275588efba737bb873ef2fd0a
joy_tailor <yegor.ivanovskyy@gmail.com>
2020-04-22 23:24:20 +0300

    Updates

---
+++ precache-manifest.ca7679764406bd289cead967a4d6e105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e31b8308355885d79123bae90b2e59",
+    "url": "/github-test-task/index.html"
+  },
+  {
+    "revision": "cb3e1190c6b95e2100db",
+    "url": "/github-test-task/static/css/2.e34f39de.chunk.css"
+  },
+  {
+    "revision": "3ed0db5983e2106665e5",
+    "url": "/github-test-task/static/css/main.913d0cbf.chunk.css"
+  },
+  {
+    "revision": "cb3e1190c6b95e2100db",
+    "url": "/github-test-task/static/js/2.ae005c80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github-test-task/static/js/2.ae005c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed0db5983e2106665e5",
+    "url": "/github-test-task/static/js/main.4572d7c3.chunk.js"
+  },
+  {
+    "revision": "6aaeba188951c16c24c9",
+    "url": "/github-test-task/static/js/runtime-main.425340a1.js"
+  }
+]);
